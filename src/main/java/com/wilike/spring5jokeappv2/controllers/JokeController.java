ac197062ce46a1a15247b8bf4a35e2bc75b32e12
wilike <wilikechoi@hotmail.com>
2021-07-18 18:13:42 +0800

    initial commit

---
+++ src/main/java/com/wilike/spring5jokeappv2/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package com.wilike.spring5jokeappv2.controllers;
+
+import com.wilike.spring5jokeappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJokes());
+        return "index";
+    }
+}
